8fdfa0c2b8720b01532b99ffdb14bba67d7fd273
hardikvyadav1122-beep <hardikvyadav1122@gmail.com>
2026-08-15 01:50:01 +0530

    Add C++ Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/162. Find Peak Element/Solution.cpp
+++ LeetCode/Patterns/Binary Search/162. Find Peak Element/Solution.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        // if(nums.size() == 1) return 0;
-        // if(nums[0] > nums[1]) return 0;
-        // if(nums[nums.size()-1] > nums[nums.size()-2]) return nums.size()-1;
-
         int low = 0;
         int high = nums.size()-1;
         while(low < high){
