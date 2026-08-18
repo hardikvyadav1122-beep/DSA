e2cc6885bb45816107bfee63b72e17feb2f56c7a
hardikvyadav1122-beep <hardikvyadav1122@gmail.com>
2026-08-18 14:58:50 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/350. Intersection of Two Arrays II/Solution.cpp
+++ LeetCode/C++/Easy/350. Intersection of Two Arrays II/Solution.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans;
+        unordered_map<int, int> mp;
+        vector<int> ans;
+
+        for(auto x : nums1){
+            mp[x]++;
+        }
+
         for(auto x : nums2){
-            if(find(nums1.begin(),nums1.end(), x) != nums1.end()){
+            if(mp[x] > 0){
                 ans.push_back(x);
-                auto it = find(nums1.begin(),nums1.end(), x);
-                if(it != nums1.end()){
-                    nums1.erase(it);
-                }
+                mp[x]--;
             }
         }
+
         return ans;
     }
 };
